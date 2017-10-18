4cca315af17009759da2ff2a8a0713221ba11eb7
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2017-10-18 14:00:32 +0200

    Temporary remove js part

--- common/js/src/main/kotlin/Main.kt
+++
@@ -1,5 +0,0 @@
-import common.fibonacci
-
-fun main(args: Array<String>) {
-    fibonacci(10)
-}
--- common/js/src/main/kotlin/common/Time.kt
+++
@@ -1,5 +0,0 @@
-package common
-
-actual class Time {
-    actual fun currentTime(): Long = 1 //TODO
-}
